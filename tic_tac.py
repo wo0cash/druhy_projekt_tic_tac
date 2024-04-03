332ee50ce9361ea3c75973b22fdbc8b9f4bd5fe0
wo0cash <139061286+wo0cash@users.noreply.github.com>
2024-04-03 12:28:58 +0200

    wlecome and rules function

--- tic_tac.py
+++ tic_tac.py
@@ -1,1 +1,27 @@
+"""
+tic_tac.py: druhý projekt do Engeto Online Python Akademie
+author: Lukasz Orszulik 
+email: luki93@seznam.cz
+discord: discord: Lukasz Orszulik, wo0cash
+"""
+def welcome():
+    """Welcome user and shows basic rules"""
+    sep = "="
+    return print(f"""
+Welcome to Tic Tac Toe
+{sep * 40}
+GAME RULES:
+Each player can place one mark or stone
+per turn on the 3x3 grid. The WINNER is
+who succeeds in placing three of their
+marks in a rows:
+                        * horizontal
+                        * vertical
+                        * diagonal
+{sep * 40}
+""")
 
+
+
+
+welcome()
